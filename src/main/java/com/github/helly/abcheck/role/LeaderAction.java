db02ca8e870c422a65beec945bbc829a1db5f53c
Helly Guo <buffoonguo@gmail.com>
2018-11-30 09:29:27 +0800

    PMD: switch->if

--- src/main/java/com/github/helly/abcheck/role/LeaderAction.java
+++ src/main/java/com/github/helly/abcheck/role/LeaderAction.java
@@ -2,6 +2,7 @@ package com.github.helly.abcheck.role;
 
 import com.github.helly.abcheck.ABStateHolder;
 import com.github.helly.abcheck.event.ABCheckEvent;
+import com.github.helly.abcheck.event.ABCheckEventType;
 import com.github.helly.abcheck.sock.PingPackage;
 
 import java.util.EventObject;
@@ -15,15 +16,9 @@ public class LeaderAction implements RoleAction {
     @Override
     public void perform(ABStateHolder holder, EventObject event) {
         ABCheckEvent abCheckEvent = (ABCheckEvent) event;
-        switch (abCheckEvent.type()) {
-            case TIMEOUT: {
-                // 定时发送心跳包
-                holder.sendReqPackage(new PingPackage());
-                break;
-            }
-            default: {
-                break;
-            }
+        if (ABCheckEventType.TIMEOUT.equals(abCheckEvent.type())) {
+            // 定时发送心跳包
+            holder.sendReqPackage(new PingPackage());
         }
     }
 }
